6f79aaffaaa7347a6f90d8f43888dab5ad637907
erickvazquez <erick.f.nunez@gmail.com>
2025-01-08 15:29:30 -0600

    Version test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to ATM Java Bank!, Version control with GIT");
     }
 }
