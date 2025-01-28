a33eb9a5d539e24d6f979318bf6280a54a63ff89
erickvazquez <erick.f.nunez@gmail.com>
2025-01-28 17:49:48 -0600

    Actualización con prueba de arrays y arraylist

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
-
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to ATM Java Bank!, Version control with GIT");
+
+        double balance  = 0.0;
+        String account = "default";
+        int pin = 123;
+
+        System.out.println("Hola mushasho");
+
+        //Ejemplo de Array en JAVA
+
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        for(int i = 0; i < arr.length; i++) System.out.println(arr[i]);
+
+        //ArrayList es dinámico, primero se tiene que importar la clase
+
+        ArrayList<Integer> myArray = new ArrayList<>();
+        myArray.add(34);
+        System.out.println(myArray.get(0));
     }
 }
